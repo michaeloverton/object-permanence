233f479690d878ae25a0b79ebe0ac1ad6f6186d9
Michael Brown <m.overtonbrown@gmail.com>
2023-02-11 13:50:55 -0800

    add toys

--- Assets/Scripts/PositionManager.cs
+++ Assets/Scripts/PositionManager.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class PositionManager : MonoBehaviour
 {
     public static PositionManager Instance { get; private set; }
-    [SerializeField] List<Transform> positions;
-    public List<TickMover> objects = new List<TickMover>();
+    [SerializeField] private bool on;
+    // private List<Transform> positions = new List<Transform>();
+    private List<TickMover> objects = new List<TickMover>();
     private Dictionary<TickMover, Transform> moverToTransform = new Dictionary<TickMover, Transform>();
-    private int registeredCount = 0;
 
     void Awake()
     {
@@ -24,39 +24,90 @@ public class PositionManager : MonoBehaviour
     public void RegisterObject(TickMover mover)
     {
         objects.Add(mover);
-        registeredCount++;
+        // positions.Add(mover.transform);
+        moverToTransform[mover] = mover.transform;
 
-        Transform tform = positions[registeredCount];
-        mover.transform.position = tform.position;
-        moverToTransform[mover] = tform;
+
+        // registeredCount++;
+
+        // Transform tform = positions[registeredCount];
+        // mover.transform.position = tform.position;
+        // moverToTransform[mover] = tform;
     }
 
     void ShuffleObjects(float t)
     {
-        bool playerIsMoving = Manager.Instance.PlayerIsMoving();
-        if(!playerIsMoving) return;
+        if(!on) return;
+        // bool playerIsMoving = Manager.Instance.PlayerIsMoving();
+        // if(!playerIsMoving) return;
         
         // Determine which objects to move.
         List<TickMover> objectsToMove = new List<TickMover>();
         List<Transform> availableTransforms = new List<Transform>();
+        // List<Vector3> availablePositions = new List<Vector3>();
         foreach(TickMover tm in objects)
         {
-            if(Random.Range(0f, 1.0f) < tm.GetProbability() && playerIsMoving)
+            // if(Random.Range(0f, 1.0f) < tm.GetProbability() && playerIsMoving)
+            if(Random.Range(0f, 1.0f) < tm.GetProbability())
             {
+                Debug.Log("object can move: " + tm.gameObject.name);
                 objectsToMove.Add(tm);
+                Debug.Log("adding " + moverToTransform[tm].position.ToString());
                 availableTransforms.Add(moverToTransform[tm]);
+                // availablePositions.Add(moverToTransform[tm].position);
                 moverToTransform.Remove(tm);
             }
         }
 
+        //DEBUG LOGS
+        foreach(Transform tr in availableTransforms)
+        {
+            Debug.Log(tr.position.ToString());
+        }
+        /// END DEBUGLOGS
+
+        Debug.Log("objects to move: " + objectsToMove.Count);
+        Debug.Log("available transforms: " + availableTransforms.Count);
+
+        // shuffle list
+        // for (int i = 0; i < availableTransforms.Count; i++) {
+        //     Transform temp = availableTransforms[i];
+        //     int randomIndex = Random.Range(i, availableTransforms.Count);
+        //     availableTransforms[i] = availableTransforms[randomIndex];
+        //     availableTransforms[randomIndex] = temp;
+        // }
+
+        // for(int i=0; i < objectsToMove.Count; i++)
+        // {
+        //     TickMover tm = objectsToMove[i];
+        //     Transform newTransform = availableTransforms[i];
+        //     tm.transform.position = newTransform.position;
+        //     moverToTransform.Add(tm, newTransform);
+        // }
+
         // Move objects.
         foreach(TickMover tm in objectsToMove)
         {
-            Transform newTransform = availableTransforms[Random.Range(0, availableTransforms.Count)];
+            Debug.Log(availableTransforms.Count);
+            int newTransformIndex = Random.Range(0, availableTransforms.Count);
+            Debug.Log("RANDOM INDEX: " + newTransformIndex);
+            Transform newTransform = availableTransforms[newTransformIndex];
             tm.transform.position = newTransform.position;
 
-            moverToTransform[tm] = newTransform;
-            availableTransforms.Remove(newTransform);
+            Debug.Log("moving " + tm.gameObject.name + " to " + newTransform.position.ToString());
+
+            // moverToTransform[tm] = newTransform;
+            moverToTransform.Add(tm, newTransform);
+            // availableTransforms.Remove(newTransform);
+            availableTransforms.RemoveAt(newTransformIndex);
+
+            Debug.Log("transforms available after removal");
+            foreach(Transform tr in availableTransforms)
+            {
+                Debug.Log(tr.position.ToString());
+            }
         }
+
+        Debug.Log("DONE://////////////////////////////////");
     }
 }
